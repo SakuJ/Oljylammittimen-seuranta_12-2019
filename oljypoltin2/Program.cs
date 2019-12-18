e412102686cd809ee0d975471296a5f4c7158220
sjj94 <t8jasa01@students.oamk.fi>
2019-12-18 16:13:16 +0200

    Initial commit

---
+++ oljypoltin2/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+
+Öljypolttimon päälläoloaikaa seuraava järjestelmä, joka tekee taustalla tekstitiedostoon lokia ajasta ja kulutuksesta ( 4L / tunti)
+Sarjadata tulee arduinolta, joka lukee öljypolttimon päälläoloa REED -putkella polttimon magneettiventtiilistä.
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace oljypoltin2
+{
+    class Program
+    {
+        static SerialPort myport;
+        
+        static void Main(string[] args)
+        {
+            int lukitus = 0;
+
+            //Sarjaportin määritys
+            myport = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
+            myport.Open();
+            
+            Stopwatch timer = new Stopwatch();
+
+            //Lokitiedoston polku .txt
+            string path = @"C:\Users\sakuj\source\repos\oljypoltin2\Testi.txt";
+
+            while (true)
+            {
+                string in_data = myport.ReadLine();
+
+                if (!int.TryParse(in_data, out int data)) ;
+
+                if (lukitus == 0 && data == 1)
+                {
+                    string appendText = DateTime.Now.ToString() + " PÄÄLLE " + Environment.NewLine ;
+                    File.AppendAllText(path, appendText);
+                    timer.Start();
+
+                    lukitus = 1;
+                }
+
+                if(lukitus == 1 && data == 0)
+                {
+                    timer.Stop();
+                    TimeSpan ts = timer.Elapsed;
+                    string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+                    float kulutus = (ts.Seconds / 3600f) * 4f;
+                    
+                    string appendText = DateTime.Now.ToString() + " POIS PÄÄLTÄ " + "Päälläoloaika: " + elapsedTime + "  Kokonaiskulutus: " + kulutus + " Litraa" + Environment.NewLine;
+                    File.AppendAllText(path, appendText);
+
+                    lukitus = 0;
+                }
+            }
+        }
+    }
+}
---
+++ oljypoltin_arduino/oljypoltin_arduino.ino
@@ -0,0 +1,42 @@
+
+const int pinSwitch = 12; //Pin Reed
+
+const int pinLed = 9; //Pin LED
+
+int StatoSwitch = 0;
+
+void setup()
+
+{
+  Serial.begin(9600);
+  
+  pinMode(pinLed, OUTPUT);
+  
+  pinMode(pinSwitch, INPUT);
+
+}
+
+void loop()
+
+{
+
+  StatoSwitch = digitalRead(pinSwitch);
+  
+  if (StatoSwitch == HIGH)
+  
+  {
+    
+    Serial.println("1");
+    digitalWrite(pinLed, HIGH);
+  
+  }
+  
+  else
+  
+  {
+    Serial.println("0");
+    digitalWrite(pinLed, LOW);
+
+  }
+
+}
